ad2ab99203faf3a60e097362d76ebddcd6b569f9
Carlos Rivas <manuelrivas75147825@gmail.com>
2024-03-08 16:26:15 -0600

    vista de login

--- src/main/java/com/example/hotel/HelloApplication.java
+++ src/main/java/com/example/hotel/HelloApplication.java
@@ -11,9 +11,10 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        stage.setTitle("Hotel");
         stage.setScene(scene);
+        stage.setResizable(false); // Evita que la ventana se pueda maximizar
         stage.show();
     }
 
